37ccb70f4ba0a74cb16c3f9b041f359548a22d18
yaoyan0988 <night840121@gmail.com>
2024-07-02 17:42:43 +0800

    處理畫面無正常顯示的問題

--- src/views/前台/forestage.vue
+++ src/views/前台/forestage.vue
@@ -16,10 +16,16 @@ export default {
 </script>
 
 <template>
-    
+    <div class="ipad">
+        <div class="showArea"></div>
+    </div>
 </template>
 
 <style scoped lang="scss">
-   
+   .ipad{
+    height: 100px;
+    width: 100px;
+    border: 1px solid black;
+   }
 
 </style>
