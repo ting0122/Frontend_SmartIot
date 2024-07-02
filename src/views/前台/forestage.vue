81e89bde62bc500ced2d524d8d86f405456e1629
yaoyan0988 <night840121@gmail.com>
2024-07-02 20:55:27 +0800

    架設前台背景

--- src/views/前台/forestage.vue
+++ src/views/前台/forestage.vue
@@ -33,30 +33,59 @@ export default {
 </script>
 
 <template>
-    <!-- this.tabsArray.buttomName是頁籤的名字陣列 可以往上拉到data看詳細內容 -->
-    <!-- 這邊透過父傳子 父是這個vue傳想要幾個內頁給BookMark BookMark會v-for傳過去的這個陣列並生成插槽 -->
-    <BookMark :tabsPresent="this.tabsArray.buttonName" :tabsName="this.tabsArray.tabsNumber">
-        <!-- 子BookMark會v-for我們所需要的插槽數量出來 -->
-        <!-- 下面template接收插槽並放入你所想要顯示的內頁資料 -->
-        <template v-slot:tab1>
-            
-        </template>
-        <template v-slot:tab2>
+    <div class="background">
+        <div class="ipad">
+            <div class="showArea">
+                <!-- this.tabsArray.buttomName是頁籤的名字陣列 可以往上拉到data看詳細內容 -->
+                <!-- 這邊透過父傳子 父是這個vue傳想要幾個內頁給BookMark BookMark會v-for傳過去的這個陣列並生成插槽 -->
+                <BookMark :tabsPresent="this.tabsArray.buttonName" :tabsName="this.tabsArray.tabsNumber">
+                    <!-- 子BookMark會v-for我們所需要的插槽數量出來 -->
+                    <!-- 下面template接收插槽並放入你所想要顯示的內頁資料 -->
+                    <template v-slot:tab1>
+                        
+                    </template>
+                    <template v-slot:tab2>
+    
+                    </template>
+                    <template v-slot:tab3>
+    
+                    </template>
+                    <template v-slot:tab4>
+    
+                    </template>
+                    <template v-slot:tab5>
+    
+                    </template>
+    
+                </BookMark>
+            </div>
+        </div>
+        
 
-        </template>
-        <template v-slot:tab3>
-
-        </template>
-        <template v-slot:tab4>
-
-        </template>
-        <template v-slot:tab5>
-
-        </template>
-
-    </BookMark>
+    </div>
 
 
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.background{
+    width: 100%;
+    height: 100dvh;
+    background-color: #636774;
+    .ipad{
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        margin-top: -550px;
+        margin-left: -800px;
+        width: 1600px;
+        height: 1100px;
+        border: 1px solid black;
+        background-image: url(../../img/ipad.png);
+        background-repeat: no-repeat;
+        background-size: contain;
+    }
+
+}
+
+</style>
