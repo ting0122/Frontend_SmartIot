228e8cf04388e623cc1cac7f8c741a301167d70c
wuuuu0410 <wu870410@mail.com>
2024-07-02 17:42:19 +0800

    作廢

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import './assets/main.css'
+import './assets/main.scss'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia} from 'pinia';
 
 import App from './App.vue'
 import router from './router'
 
-const pinia = createPinia()
 const app = createApp(App)
+const pinia = createPinia();
 
 app.use(router)
-
 app.use(pinia)
+
 app.mount('#app')
