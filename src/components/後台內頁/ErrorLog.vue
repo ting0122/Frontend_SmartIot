f3f466d8fa254bdb728906c31af06e2b7be07e3e
yaoyan0988 <night840121@gmail.com>
2024-07-06 19:19:54 +0800

    後台錯誤紀錄頁面設置調整完成+房間單元細節設置完成+設備單元細節設置完成

--- src/components/後台內頁/ErrorLog.vue
+++ src/components/後台內頁/ErrorLog.vue
@@ -1,9 +1,9 @@
 <script>
-import CategoryButton from '@/components/XCategoryButton.vue';
+import SearchError from '@/components/SearchError.vue';
 export default {
     data() {
         return {
-            divArr: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            dataArr:[{id:203154,type:"冷氣",mane:"前方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203151,type:"電燈",mane:"右側電燈",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"南方麒麟股份有限公司"},{id:203157,type:"空氣清淨機",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"},{id:203157,type:"冷氣",mane:"後方的冷氣",question:"網路連線中斷",date:"2024-06-02",area:602,roommane:"會議室"}]
         };
     },
     created() {
@@ -16,7 +16,7 @@ export default {
 
     },
     components: {
-        CategoryButton
+        SearchError
     },
 
     methods: {
@@ -26,43 +26,36 @@ export default {
 </script>
 
 <template>
-    <div class="area">
-        <CategoryButton/>
-        <div class="lineArea">
-            <p>設備故障紀錄</p>
-            <div class="line"></div>
-        </div>
-        <div class="history" v-for="(div, index) in divArr">
+    <div class="out">
+        <SearchError/>
+        
+        <div class="down">
+            <div class="history" v-for="(data, index) in dataArr">
+                <div class="box">
+                    <h2>{{ data.area }}-{{ data.roommane }}</h2>
+                    <p class="id">{{ data.id }}</p>
+                    <p>{{ data.type }}</p>
+                    <p>{{ data.mane }}</p>
+                    <p class="right">{{ data.question }}</p>
+                </div>
+                <p class="date">{{ data.date }}</p>
+            </div>
+
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 @import '@/assets/main.scss';
-.area {
-    width: 1200px;
-    height: 926px;
+.out {
+    width: 1238px;
+    height: 100%;
     display: flex;
     flex-direction: column;
-    overflow-y: auto;
-    &::-webkit-scrollbar {
-        width: 7px;
-    }
-
-    &::-webkit-scrollbar-button {
-        background: transparent;
-    }
-
-    &::-webkit-scrollbar-thumb {
-        background: black;
-        border-radius: 15px;
-
-    }
-
-    &::-webkit-scrollbar-track {
-        background: #ffffff;
-        border-radius: 15px;
-    }
+    align-items:end;
+    border-radius: 25px;
+    border: 1px solid black;
+    
 }
 .lineArea {
     width: 1040px;
@@ -83,11 +76,77 @@ export default {
         color: $white;
     }
 }
-.history {
-    width: 1170px;
-    height: 57px;
-    margin: 0 0 20px 0;
-    background: $dark03;
-    border-radius: 35px;
+.down{
+    width: 100%;
+    height: 825px;
+    overflow-y: auto;
+    border: 1px solid black;
+    border-radius: 0 0 25px 25px;
+    background-color: $dark01;
+    padding-top: 10px;
+    &::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    &::-webkit-scrollbar-button {
+        background: transparent;
+        height: 30px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: $black1;
+        border-radius: 15px;
+
+    }
+
+    &::-webkit-scrollbar-track {
+        background: transparent;
+        border-radius: 15px;
+    }
+    .history {
+        width: 1170px;
+        height: 57px;
+        margin: 20px auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background: $dark03;
+        border-radius: 35px;
+        
+        .box{
+            // width: 305px;
+            height: 30px;
+            display: flex;
+            padding-left: 10px;
+            h2{
+                display: block;
+                font-size: 18px;
+                line-height: 30px;
+                margin-left: 23px;
+                margin-right: 25px;
+                color: $white;
+                    
+            }
+            p{
+                // font-size: 1px;
+                line-height: 30px;
+                margin-right: 18px;
+                color: $white;
+            }
+            .right{
+                display: block;
+                margin-left: 8px;
+                padding-left: 28px;
+                // width: 500px;
+                color: $white;
+                border-left: 1px solid $white;
+            }
+        }
+        .date{
+            display: block;
+            margin-right: 30px;
+            color: $white;
+        }
+    }
 }
 </style>
